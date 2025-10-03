925ce8d0d95cae6a1cda250e8db2c1a7117a3998
David L. <36916479+davidfrosch@users.noreply.github.com>
2025-10-03 15:57:01 +0200

    Modify book description for 'Impressions'
    Updated the description of 'Impressions' book.

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function escapeHtml(str) {
 
 const books = [
   { cover: 'images/book1-cover.jpg', pages: 10, title: 'David Frisch - Thank You', description: 'A kaleidoscopic perspective. 172 pages.', binding: 'A4 - Hardcover', price: '€20' },
-  { cover: 'images/book2-cover.jpg', pages: 10, title: 'David Frisch - Impressions', description: 'Time is slipping by. 126 pages.', binding: 'A4 - Hardcover', price: '€15' },
+  { cover: 'images/book2-cover.jpg', pages: 10, title: 'David Frisch - Impressions', description: 'Time keeps on slipping by. 126 pages.', binding: 'A4 - Hardcover', price: '€15' },
   { cover: 'images/book3-cover.jpg', pages: 10, title: 'David L. & Vinzenz K. - Appalachia Roadtrip', description: 'Documenting a road trip through rural Appalachia. 336 pages.', binding: 'A5 - Hardcover', price: '€15' }
 ];
 
